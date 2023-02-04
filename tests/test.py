0e72d1f45e4c9f80b859e78a44dd6a011cbb4d26
Sagar <sagarxyadav@gmail.com>
2023-02-03 20:30:45 -0500

    mv

---
+++ tests/test.py
@@ -0,0 +1,17 @@
+import django
+django.setup()
+
+from django.test import TestCase
+from crypto.forms import EncryptionForm, DecryptionForm
+
+class EncryptionFormTestCase(TestCase):
+    def test_form_has_fields(self):
+        form = EncryptionForm()
+        expected_fields = ['text']
+        self.assertSequenceEqual(expected_fields, list(form.fields))
+
+class DecryptionFormTestCase(TestCase):
+    def test_form_has_fields(self):
+        form = DecryptionForm()
+        expected_fields = ['text']
+        self.assertSequenceEqual(expected_fields, list(form.fields))
