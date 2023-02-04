9ff89e09bd871eb32a176dacde3988e72b9c3b70
Sagar <sagarxyadav@gmail.com>
2023-02-04 00:35:55 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python
-import django
-django.setup()
 import os
 import sys
 from django.conf import settings
 
-settings.configure(default_settings=__name__)
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crypto.settings")
+    settings.configure(default_settings=__name__)
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
